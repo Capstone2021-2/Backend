c74ea573c2ddb1d791fe6783af87f1d4f1671a04
Yoo Chang-Seon <plusminus54@gmail.com>
2021-10-13 18:17:25 +0900

    chang password

--- pillsogood/pillsogood/settings.py
+++ pillsogood/pillsogood/settings.py
@@ -159,4 +159,11 @@ AUTHENTICATION_BACKENDS = (
     ('django.contrib.auth.backends.ModelBackend'),
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = my_settings.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = my_settings.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
